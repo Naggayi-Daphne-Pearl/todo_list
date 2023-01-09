2c06e734d58ce35074e36929d07750acd4bd1ee7
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-01-09 09:39:46 -0800

    render template

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    # Rendering index page
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
