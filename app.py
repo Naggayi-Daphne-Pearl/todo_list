520d5799ba4bfd46eadd2f702504c6800e799a59
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-01-11 03:08:54 -0800

    gitignore

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/postgres'                                                                                                        
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
-
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,13 +23,23 @@ def index():
 
         try:
             db.session.add(new_task)
-            db.commit()
+            db.session.commit()
             return redirect('/')
         except:
-            return 'There was an issue adding your task. Please try again'
+            return 'There was an issue adding your task'
+
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
